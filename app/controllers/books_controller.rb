a844e616c2fc77c50635033ff47ffb4a190fc0a1
tammy thompson <thompson_tammy@bah.com>
2014-03-25 21:57:25 -0400

    added showing a book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,5 +2,8 @@ class BooksController <ApplicationController
    def index
      @available_at = Time.now
      @books = Book.all
+   def show
+  @book = Book.find(params[:id])
+     end
    end
 end
