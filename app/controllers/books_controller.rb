6536bac3711341eab16fd72301b7aac5c3d6e9c9
tammy thompson <thompson_tammy@bah.com>
2014-03-25 21:36:11 -0400

    added display books from database

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController <ApplicationController
    def index
-   @available_at = Time.now
-   @books = ["Atalas Shrugged", "Ender's Game", "Hamlet", "The Hobbit"]
+     @available_at = Time.now
+     @books = Book.all
    end
 end
