b6db169e769742c98c5c7214ad5a71a86a0baf44
tammy.thompson <tjgibson4@gmail.com>
2014-04-01 21:03:25 -0400

    Class 09 Lab 5 - Destroying a Book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController <ApplicationController
+   before_action :set_book, only: [ :show, :edit, :update, :destroy ]
    def index
      @available_at = Time.now
      @books = Book.all
@@ -24,9 +25,16 @@ class BooksController <ApplicationController
      @book.update(book_params)
      redirect_to @book
    end
-   private
-
+   def destroy
+     @book = Book.find(params[:id])
+     @book.destroy
+     redirect_to books_url
+   end
+ private
    def book_params
      params.require(:book).permit(:title, :author, :pages, :price)
+   end
+   def set_book
+     @book = Book.find(params[:id])
    end
 end
