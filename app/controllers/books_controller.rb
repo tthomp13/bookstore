f1941845eb78d0261d032e4fd44681a26d050f66
tammy.thompson <tjgibson4@gmail.com>
2014-04-01 20:39:45 -0400

    Class 09 Lab4- Editing a book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,31 @@ class BooksController <ApplicationController
    def index
      @available_at = Time.now
      @books = Book.all
+   end
    def show
   @book = Book.find(params[:id])
      end
+   def new
+     @book = Book.new
+   end
+
+   def create
+     @book = Book.new(book_params)
+     @book.save
+     redirect_to @book
+   end
+   def edit
+     @book = Book.find(params[:id])
+   end
+
+   def update
+     @book = Book.find(params[:id])
+     @book.update(book_params)
+     redirect_to @book
+   end
+   private
+
+   def book_params
+     params.require(:book).permit(:title, :author, :pages, :price)
    end
 end
